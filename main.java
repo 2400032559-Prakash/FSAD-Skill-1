42e1f9c34cdb880eab2471bcd3061947b5d84767
Prakash <prakash10dtg@gmail.com>
2026-04-06 06:10:58 +0530

    Update with proper java program

--- main.java
+++ main.java
@@ -1,7 +1,8 @@
-Hello Git
-
-Feature update added
-
-Bug fixed here
-
-This is the main class
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("Feature update added");
+        System.out.println("Bug fixed here");
+        System.out.println("This is the main class");
+    }
+}
